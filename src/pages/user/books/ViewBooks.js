45700c2c70794da17ec75dd68149bee55a3dea23
Tim <tmetcalfe89@gmail.com>
2023-03-08 14:51:21 -0800

    changed the layout of the book view

--- src/pages/user/books/ViewBooks.js
+++ src/pages/user/books/ViewBooks.js
@@ -3,15 +3,23 @@ import { Link } from "react-router-dom";
 
 export default function ViewBooks({ books }) {
   return (
-    <Group>
+    <Group align="flex-end">
       {books.map((book) => (
         <Link to={`/books/edit/${book.id}`}>
-          <Tooltip label={book.title} position="bottom">
+          <Tooltip label={`${book.title} by ${book.author}`} position="bottom">
             <Image src={book.image} maw={250} />
           </Tooltip>
         </Link>
       ))}
-      <Button component={Link} to="/books/create">
+      <Button
+        component={Link}
+        to="/books/create"
+        style={{
+          alignSelf: "stretch",
+          height: "initial",
+          minHeight: "2.25rem",
+        }}
+      >
         Add a Book
       </Button>
     </Group>
